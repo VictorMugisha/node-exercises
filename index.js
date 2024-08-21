2b2e8df58baf89d1419534c8c44b94162bffb9fd
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 10:25:03 +0200

    Reading file synchronously

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const fs = require("fs")
 
 fs.writeFileSync("./greet.txt", "Hello Victor")
 
-fs.writeFile("./greet.txt", "Hello Victor", (err) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("File written successfully")
-    }
-})
+// fs.writeFile("./greet.txt", "Hello Victor", (err) => {
+//     if (err) {
+//         console.log(err)
+//     } else {
+//         console.log("File written successfully")
+//     }
+// })
+
+
+// Reading File Synchronously
+const fileContent = fs.readFileSync("./greet.txt", "utf-8")
+console.log(fileContent)
