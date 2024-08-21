d7b7a3c0d0f91ceeb29f45e464a53ece7319f315
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 10:31:26 +0200

    Reading files using Promise API on file system module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const fsPromises = require("fs").promises
 
 fs.writeFileSync("./greet.txt", "Hello Victor")
 
@@ -16,9 +17,21 @@ const fileContent = fs.readFileSync("./greet.txt", "utf-8")
 // console.log(fileContent)
 
 // Readinf File Asynchronously
-fs.readFile("./greet.txt", "utf-8", (error, data) => {
-    if (error) 
-        console.log(error)
-    else 
-        // console.log("Data from file: ", data)
-})
+// fs.readFile("./greet.txt", "utf-8", (error, data) => {
+//     if (error) 
+//         console.log(error)
+//     else 
+//         console.log("Data from file: ", data)
+// })
+
+// Reading file using promises
+async function readFile () {
+    try {
+        const data = await fsPromises.readFile("./greet.txt", "utf-8")
+        console.log(data)
+    } catch(err) {
+        console.log("ERROR: ", err)
+    }
+}
+
+readFile()
