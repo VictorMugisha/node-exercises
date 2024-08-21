00daf97fc77ac217cdc32712def3a64c176b95b9
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 10:27:02 +0200

    Reading File Asynchronously

--- index.js
+++ index.js
@@ -13,4 +13,12 @@ fs.writeFileSync("./greet.txt", "Hello Victor")
 
 // Reading File Synchronously
 const fileContent = fs.readFileSync("./greet.txt", "utf-8")
-console.log(fileContent)
+// console.log(fileContent)
+
+// Readinf File Asynchronously
+fs.readFile("./greet.txt", "utf-8", (error, data) => {
+    if (error) 
+        console.log(error)
+    else 
+        // console.log("Data from file: ", data)
+})
