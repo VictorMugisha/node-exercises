7d17f0542f816a3fc6e87a2101b5e6b4358ca3b5
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 16:32:22 +0200

    Another example of event emitter

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const EventEmitter = require("events")
 
 const myEventEmitter = new EventEmitter()
@@ -7,3 +8,22 @@ myEventEmitter.on("victor", (greet, name) => {
 })
 
 myEventEmitter.emit("victor", "Hello", "JavaScript")
+
+
+// Another example with file reading
+const myEmitter = new EventEmitter();
+
+// Set up a listener for 'fileRead' event
+myEmitter.on('fileRead', (data) => {
+    console.log('File content:', data);
+});
+
+// Read file asynchronously
+fs.readFile('example.txt', 'utf8', (err, data) => {
+    if (err) {
+        myEmitter.emit('error', err);
+        return;
+    }
+    // Emit 'fileRead' event with file data
+    myEmitter.emit('fileRead', data);
+});
